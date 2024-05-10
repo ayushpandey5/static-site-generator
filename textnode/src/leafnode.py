9ad0908204fc291df0126e02bcfc40625bc6a01b
Ayush Pandey <ayushpandey5@icloud.com>
2024-05-10 17:19:34 +1000

    leafnode

---
+++ textnode/src/leafnode.py
@@ -0,0 +1,16 @@
+from htmlnode import HTMLNode
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag=None, value=None, props=None) -> None:
+        super().__init__(tag=tag, value=value, props=props)
+
+    def to_html(self):
+        if self.value == None:
+            raise ValueError("No value")
+        if self.tag == None:
+            return self.value
+        if self.props != None:
+            prop_string = self.props_to_html()
+        return f"<{self.tag}{' '+ prop_string if prop_string else ''}>{self.value}</{self.tag}>"
+
+
