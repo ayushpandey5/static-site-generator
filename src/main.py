a847ce41de00a0098fb296b9a0f707b283afd333
Ayush Pandey <theayushpandey2057@gmail.com>
2024-12-08 13:31:17 +1100

    copy all static files to public

--- src/main.py
+++ src/main.py
@@ -1,6 +1,22 @@
-from textnode import *
+import os
+import shutil
 
+base_path = '.'
+def main():
+    if os.path.exists(os.path.join(base_path, 'public')):
+        shutil.rmtree(os.path.join(base_path, 'public'))
+    os.mkdir(os.path.join(base_path, 'public'))
+    copy_files(os.path.join(base_path, 'static'), os.path.join(base_path, 'public'))
+        
 
-node1 = TextNode("This is a text node", TextType.BOLD, "https://www.boot.dev")
-
-print(node1)
+def copy_files(source_path, dest_path):
+    items = os.listdir(source_path)
+    for item in items:
+        if os.path.isfile(os.path.join(source_path, item)):
+            shutil.copy(os.path.join(source_path, item), dest_path)
+        else:
+            dest_dir = os.path.join(dest_path, item)
+            os.mkdir(dest_dir)
+            copy_files(os.path.join(source_path, item), dest_dir)
+        print(f"Copied {item} to {dest_path}")
+main()
