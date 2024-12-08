0cc398d141f796ab7691a9feab12b8ebe66ff322
Ayush Pandey <theayushpandey2057@gmail.com>
2024-12-08 19:19:24 +1100

    leafnode and convert leafnode to html

--- src/htmlnode.py
+++ src/htmlnode.py
@@ -14,3 +14,17 @@ class HTMLNode():
             prop_string += (f' {key}: "{val}"')
         return prop_string
 
+
+
+class LeafNode(HTMLNode):
+    def __init__(self, tag, value, props):
+        super().__init__(tag, value, props)
+    
+    def to_html(self):
+        if self.value is None:
+            raise ValueError("All leaf nodes must have a value")
+        if self.tag is None:
+            return f"{self.value}"
+        return f'<{self.tag}{self.props_to_html()}>{self.value}</{self.tag}>'
+    
+    
