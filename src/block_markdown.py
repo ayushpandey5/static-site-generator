7d5cd2acdf781393b5032e49278cf96951a7c3db
Ayush Pandey <theayushpandey2057@gmail.com>
2024-12-12 10:04:45 +1100

    split markdown file to blocks

---
+++ src/block_markdown.py
@@ -0,0 +1,4 @@
+def markdown_to_blocks(markdown):
+    blocks = markdown.split('\n\n')
+    blocks = list(map(lambda block: block.strip(),filter(lambda block: block != "", blocks)))
+    return blocks
