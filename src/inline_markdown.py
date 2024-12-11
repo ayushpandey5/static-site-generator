c286b6c50288d4ef49a200e52bb64ebd7e5f8175
Ayush Pandey <theayushpandey2057@gmail.com>
2024-12-12 07:23:50 +1100

    split text to text nodes

--- src/inline_markdown.py
+++ src/inline_markdown.py
@@ -77,3 +77,14 @@ def split_nodes_link(old_nodes):
     return new_nodes
 
 
+def text_to_textnodes(text):
+    nodes = [TextNode(text, TextNodeType.TEXT)]
+    nodes = split_nodes_link(nodes)
+    nodes = split_nodes_image(nodes)
+    nodes = split_nodes_delimiter(nodes, "**", TextNodeType.BOLD)
+    nodes = split_nodes_delimiter(nodes, "*", TextNodeType.ITALIC)
+    nodes = split_nodes_delimiter(nodes, "`", TextNodeType.CODE)
+    return nodes
+
+text = "This is **text** with an *italic* word and a `code block` and an ![obi wan image](https://i.imgur.com/fJRm4Vk.jpeg) and a [link](https://boot.dev)"
+text_to_textnodes(text)
