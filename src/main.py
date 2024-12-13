401821a40a873face2120f382889daa7a2d76b54
Ayush Pandey <theayushpandey2057@gmail.com>
2024-12-13 19:20:37 +1100

    copy static files to public recursively

---
+++ src/main.py
@@ -0,0 +1,26 @@
+import os
+import shutil
+
+base_path = os.path.dirname(os.path.dirname(__file__))
+
+def main():
+    if "public" in os.listdir(base_path):
+        shutil.rmtree(os.path.join(base_path, 'public'))
+    os.mkdir(os.path.join(base_path, 'public'))
+    static_path = os.path.join(base_path, 'static')
+    dest_path = os.path.join(base_path, 'public')
+    copy_file(static_path, dest_path)
+
+        
+
+def copy_file(source_path, dest_path):
+    files = os.listdir(source_path)
+    for file in files:
+        file_path = os.path.join(source_path, file)
+        if os.path.isfile(file_path):
+            shutil.copy(file_path, dest_path)
+        else:
+            os.mkdir(os.path.join(dest_path, f'{file}'))
+            copy_file(file_path, os.path.join(dest_path, f'{file}'))
+    
+main()
